1311a9edcd2f829fde2a1bc9c9b32380229f3127
Jan <janphi.bean@gmail.com>
2025-06-10 12:00:16 +0200

    feat: default image for themes

--- packages/themes/shr-sssystem-core/setup.sh
+++ packages/themes/shr-sssystem-core/setup.sh
@@ -1,7 +1,12 @@
 load_setup() {
     echo "loading sssystem core setup..."
     if gum confirm --default=true "Set Desktop wallpaper? (Some themes require a Wallpaper)"; then
-    wallpaper_path=$(gum input --placeholder "Enter path to wallpaper")
+
+    DEFAULT_PATH="themes/sssystem-theme/dotfiles/.config/hypr/resources/ruan-jia.jpg"
+    wallpaper_path=$(gum input --placeholder "$DEFAULT_PATH" --value "$DEFAULT_PATH" --prompt "Enter path to wallpaper: (default = $DEFAULT_PATH")
+
+    # Fallback if user enters nothing (just presses Enter)
+    wallpaper_path="${wallpaper_path:-$DEFAULT_PATH}"
     if [ -n "$wallpaper_path" ]; then
       ask_execute wal -i "$wallpaper_path"
     else
