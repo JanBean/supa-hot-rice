9de0bb9a4b3067363f9de363b51df83043579754
Jan <janphi.bean@gmail.com>
2025-06-10 10:18:17 +0200

    feat: repo branch und user von url als parameter erlauben

--- setup.sh
+++ setup.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-# Online script for install supa-hot-rice.
 
-me="Supa Hot Rice"
-remote_repo=JanBean/supa-hot-rice
-branch=theme-rework
-
-set -e
+# Online script to install supa-hot-rice.
 
 function try { "$@" || sleep 0; }
 function x() {
   if "$@";then cmdstatus=0;else cmdstatus=1;fi # 0=normal; 1=failed; 2=failed but ignored
   while [ $cmdstatus == 1 ] ;do
-    echo -e "\e[31m$me: Command \"\e[32m$@\e[31m\" has failed."
+    echo -e "\e[31m$REPO: Command \"\e[32m$@\e[31m\" has failed."
     echo -e "You may need to resolve the problem manually BEFORE repeating this command.\e[0m"
     echo "  r = Repeat this command (DEFAULT)"
     echo "  e = Exit now"
@@ -24,35 +19,50 @@ function x() {
     esac
   done
   case $cmdstatus in
-    0) echo -e "\e[34m$me: Command \"\e[32m$@\e[34m\" finished.\e[0m";;
-    1) echo -e "\e[31m$me: Command \"\e[32m$@\e[31m\" has failed. Exiting...\e[0m";exit 1;;
+    0) echo -e "\e[34m$REPO: Command \"\e[32m$@\e[34m\" finished.\e[0m";;
+    1) echo -e "\e[31m$REPO: Command \"\e[32m$@\e[31m\" has failed. Exiting...\e[0m";exit 1;;
   esac
 }
 
+set -e
+
 command -v pacman || { echo "\"pacman\" not found. This script only work for Arch(-based) Linux distros. Aborting..."; exit 1 ; }
 
-if [ -z "$1" ]; then
-  path=~/supa-hot-rice
+REPO="supa-hot-rice"
+BRANCH="master"
+REMOTE_REPO="JanBean/${REPO}"
+
+# optional use script url data passed as argument
+SCRIPT_URL="$1"
+if [[ $SCRIPT_URL =~ raw\.githubusercontent\.com/([^/]+)/([^/]+)/refs/heads/([^/]+) ]]; then
+    USER="${BASH_REMATCH[1]}"
+    REPO="${BASH_REMATCH[2]}"
+    BRANCH="${BASH_REMATCH[3]}"
+    REMOTE_REPO="${USER}/${REPO}"
+
+    echo -e "\e[34mInstalling from $REMOTE_REPO@$BRANCH \e[0m"
 else
-  path="$1"
+    echo -e "\e[31mERROR: Could not extract repository metadata, using default values \e[0m"
 fi
+
+path="${HOME}/${REPO}"
 
 # Check if git is installed, install if not
 if ! command -v git >/dev/null; then
-  echo -e "\e[34m$me: Git is not installed. Installing with pacman....\e[0m"
+  echo -e "\e[34m$REPO: Git is not installed. Installing with pacman....\e[0m"
   x sudo pacman -Sy --noconfirm git
 fi
 
-echo "$me: Downloading repo to $path ..."
-x mkdir -p $path
-x cd $path
+echo "$REPO: Downloading repo to $path ..."
+x mkdir -p "$path"
+x cd "$path"
 if [ -z "$(ls -A)" ]; then
-  x git init -b $branch
-  x git remote add origin https://github.com/$remote_repo
+  x git init -b "$BRANCH"
+  x git remote add origin https://github.com/"$REMOTE_REPO"
 fi
-git remote get-url origin|grep -q $remote_repo || { echo "Dir \"$path\" is not empty, nor a git repo of $remote_repo. Aborting..."; exit 1 ; }
-x git pull origin $branch && git submodule update --init --recursive
-echo "$me: Downloaded."
-echo "$me: Running \"install.sh\"."
+git remote get-url origin|grep -q "$REMOTE_REPO" || { echo "Dir \"$path\" is not empty, nor a git repo of $REMOTE_REPO. Aborting..."; exit 1 ; }
+x git pull origin "$BRANCH" && git submodule update --init --recursive
+echo "$REPO: Downloaded."
+echo "$REPO: Running \"install.sh\"."
 x ls -a
-x ./install.sh || { echo "$me: Error occured when running \"install.sh\"."; exit 1 ; }
+x ./install.sh || { echo "$REPO: Error occurred when running \"install.sh\"."; exit 1 ; }
