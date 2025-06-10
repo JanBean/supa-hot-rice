4f55ce67c9b378d390aaa5bf12beafb976da275b
Jan <janphi.bean@gmail.com>
2025-06-10 12:04:51 +0200

    fix: wallpaper setup

--- packages/themes/shr-sssystem-core/setup.sh
+++ packages/themes/shr-sssystem-core/setup.sh
@@ -3,7 +3,7 @@ load_setup() {
     if gum confirm --default=true "Set Desktop wallpaper? (Some themes require a Wallpaper)"; then
 
     DEFAULT_PATH="themes/sssystem-theme/dotfiles/.config/hypr/resources/ruan-jia.jpg"
-    wallpaper_path=$(gum input --placeholder "$DEFAULT_PATH" --value "$DEFAULT_PATH" --prompt "Enter path to wallpaper: (default = $DEFAULT_PATH")
+    wallpaper_path=$(gum input --placeholder "$DEFAULT_PATH" --prompt "Enter path to wallpaper: (default = $DEFAULT_PATH")
 
     # Fallback if user enters nothing (just presses Enter)
     wallpaper_path="${wallpaper_path:-$DEFAULT_PATH}"
